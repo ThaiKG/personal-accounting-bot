b548d009c66c661b66b506e55a1217b06f054263
Thai Truong <thaitruongdev@gmail.com>
2026-01-27 14:25:42 -0600

    Add schema for each bill or expense

---
+++ schemas/Expense.js
@@ -0,0 +1,35 @@
+const { AutoModerationRule } = require("discord.js");
+const { Schema, model } = require("mongoose");
+
+const expenseSchema = new Schema({
+    paidBy: {
+        type: String,
+        required: true,
+    },
+    amount: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: false,
+    },
+    participants: {
+        type: [String],
+        required: true,
+    },
+    splitAmount: {
+        type: Number,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    settled: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+module.exports = model("Expense", expenseSchema);
